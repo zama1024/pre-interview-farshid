2e697ec2178522665a7bd727a7e1727e39562369
zama1024 <farshid_bd@hotmail.com>
2017-08-19 15:55:39 -0400

    Add String#endsWith

--- string.js
+++ string.js
@@ -1,3 +1,14 @@
 String.prototype.endsWith = function(search, position) {
-  return false
-}
+  let endPos = position ? position - 1 : this.length - 1;
+
+  //-------------------------One liner Solution----------------------------
+  // return this.slice(endPos - search.length + 1,endPos + 1) === search;
+
+  //------------------------Alternate Solution-----------------------------
+  let compareStr = '';
+  for(var i = endPos; i > endPos - search.length ; i--){
+    compareStr = this[i] + compareStr;
+  }
+  console.log(compareStr);
+  return compareStr === search;
+};
